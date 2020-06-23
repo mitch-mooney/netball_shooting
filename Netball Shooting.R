58536bdb274b00ecb782175a45377451c0a6c1cd
Mitchell Mooney <mitchellgmooney@gmail.com>
2020-06-23 15:10:43 +1000

    Netball shot data with plots

---
+++ Netball Shooting.R
@@ -0,0 +1,85 @@
+library(ggplot2)
+library(plotly)
+library(ggpmisc)
+library(dplyr)
+
+shots <- read.csv('netball_shots.csv')
+
+# plot to see if home or away predictions are more effective
+plotly_build(shots%>%
+               mutate(shotResult = ifelse(scorepoints == 1, "Goal", "Miss")) %>% 
+               ggplot(aes(x = shotResult, fill = shotResult))+
+               geom_bar(aes(y = (..count..)/sum(..count..)))+
+               ggtitle('Goal shooting percentage for all attempts since 2009')+
+               ylab("percentage of all shots")+
+               xlab('Attempt result'))
+
+# long shots percentage
+plotly_build(shots %>%
+  filter((positionCode == 2 & distanceCode == 3) | (positionCode == 2 & distanceCode == 1) | 
+           (positionCode == 1 & distanceCode == 1) | (positionCode == 0 & distanceCode == 1) |
+           (positionCode == 0 & distanceCode == 3)) %>% 
+  mutate(shotResult = ifelse(scorepoints == 1, "Goal", "Miss")) %>% 
+ggplot(aes(x = shotResult, fill = shotResult))+
+  geom_bar(aes(y = (..count..)/sum(..count..)))+
+  ggtitle('Long goal shooting percentage for all attempts since 2009')+
+  ylab("percentage of all long shots")+
+  xlab('Attempt result'))
+
+# Percent of shots taken at distance
+plotly_build(shots %>%
+               mutate(shotDistance = ifelse(positionCode == 2 & distanceCode == 3, "long", ifelse(positionCode == 2 & distanceCode == 1, "long", 
+                        ifelse(positionCode == 1 & distanceCode == 1, "long", ifelse(positionCode == 0 & distanceCode == 1, "long",
+                        ifelse(positionCode == 0 & distanceCode == 3, "long", "short")))))) %>% 
+               #mutate(shotResult = ifelse(scorepoints == 1, "Goal", "Miss")) %>% 
+               ggplot(aes(x = shotDistance, fill = shotDistance))+
+               geom_bar(aes(y = (..count..)/sum(..count..)))+
+               ggtitle('Long v short goal attempts since 2009')+
+               ylab("percentage of all shots")+
+               xlab('Attempt distancet'))
+
+#create a position of the shot based on distance and position codes
+shots <- shots %>%
+  mutate(shot_pos = ifelse(positionCode == 2 & distanceCode == 3, 1, 
+                           ifelse(positionCode == 2 & distanceCode == 1, 2,
+                                  ifelse(positionCode == 1 & distanceCode == 1, 3,
+                                         ifelse(positionCode == 0 & distanceCode == 1, 4, 5)))))
+#summarize shots by position
+shot_summary<-shots %>%
+  group_by(shot_pos, displayName) %>%
+  summarise(goals = sum(scorepoints), attempts = n(), perc = goals/attempts)
+#provide an x and y coordinate for the shot summary
+shot_summary <- shot_summary %>%
+  mutate(x = ifelse(shot_pos == 1, -4,
+                    ifelse(shot_pos == 2, -3.5,
+                           ifelse(shot_pos == 3, 0,
+                                  ifelse(shot_pos == 4, 3.5, 4))))) %>%
+  mutate(y = ifelse(shot_pos == 1, 0.25,
+                    ifelse(shot_pos == 2, 1.8,
+                           ifelse(shot_pos == 3, 3.5,
+                                  ifelse(shot_pos == 4, 1.8, 0.25)))))
+
+shot_summary$perc <- round(shot_summary$perc,2) # round percentage to 2 decimal points
+
+# plot the display of long shots
+shot_summary %>%
+  filter(displayName == "E.Bell")%>% #choose a different player
+  ggplot(aes(x = x, y = y))+
+  geom_text(aes(label=paste("(",goals,",",attempts,")",perc)))+
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank(),
+        axis.title.y=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank(),
+        panel.background = element_rect(fill = "white", colour = "grey50"))+
+  geom_path(data = court, aes(Y, X - 2.805), colour = "black", size = 1)+
+  geom_path(data = court, aes(Y/2, X/2-1.4), colour = "black", size = 1)+
+  #Baseline shot Line (10.16m
+  geom_segment(aes(x = -4.9, y = 0.5, xend = 4.9, yend = 0.5), lwd = 1, color = "black") +
+  #outter segements
+  geom_segment(aes(x = 0, y = 0, xend = 2.5, yend = 4.24), lwd = 1, color = "black") +
+  #outter segements
+  geom_segment(aes(x = 0, y = 0, xend = -2.5, yend = 4.24), lwd = 1, color = "black")+
+  ggtitle("Long shot chart: E.Bell",
+          subtitle = "(goals, attempts) percentage")
